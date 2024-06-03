6742a37e8ed9bb14be6f0766cb6d328b33e72f5b
patpatpanda <nils-emil1337@hotmail.se>
2024-06-03 14:32:58 +0200

    f

--- MasterKinder/Program.cs
+++ MasterKinder/Program.cs
@@ -7,15 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"),
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5, // Max antal återförsök
+            maxRetryDelay: TimeSpan.FromSeconds(30), // Maximal fördröjning mellan återförsök
+            errorNumbersToAdd: null // Felnummer att inkludera i återförsök
+        );
+    }));
 
 builder.Services.AddScoped<CsvService>();
 builder.Services.AddSingleton<MasterKinder.Services.AuthService>();
 builder.Services.AddSingleton<MasterKinder.Services.PowerBIService>();
+
 var app = builder.Build();
 
 // Load CSV Data
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,6 +31,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+
 // https://virki.se/
 app.UseHttpsRedirection();
 app.UseStaticFiles();
