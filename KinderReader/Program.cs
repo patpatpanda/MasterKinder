5fb182833afffc1bdde047a1bd8d18adbe8b91cb
patpatpanda <nils-emil1337@hotmail.se>
2024-06-26 16:22:23 +0200

    a

--- KinderReader/Program.cs
+++ KinderReader/Program.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,10 +18,6 @@ builder.Services.AddDbContext<MrDb>(options =>
 
 // Add the scraper service
 builder.Services.AddScoped<Scraper>();
-
-// Add the PDF scraping service
-builder.Services.AddHttpClient();
-builder.Services.AddTransient<PdfScrapingService>();
 
 var app = builder.Build();
 
@@ -45,22 +39,32 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Run the PDF scraping service
-await RunPdfScraperAsync(app.Services);
+// Run the scraper with start and end IDs
+await RunScraperAsync(app.Services, 10500, 10700);
+
+// Remove duplicates
+await RemoveDuplicatesAsync(app.Services);
 
 app.Run();
 
-async Task RunPdfScraperAsync(IServiceProvider services)
+async Task RunScraperAsync(IServiceProvider services, int startId, int endId)
 {
     using var scope = services.CreateScope();
-    var pdfScrapingService = scope.ServiceProvider.GetRequiredService<PdfScrapingService>();
-    try
-    {
-        // Pass your actual URL here
-        await pdfScrapingService.ScrapeAndSavePdfData("https://ssan.stockholm.se/anonym/webdokument/Delade%20dokument/Forms/AllItems.aspx?RootFolder=%2Fanonym%2Fwebdokument%2FDelade%20dokument%2FF%C3%B6rskolor%2F2024%2FS%C3%B6dermalm&FolderCTID=0x01200015B00A3B7947284E8A98F455403CF440&View=%7BCEB0BF65%2D2CB1%2D4A7B%2DA2B3%2DD82EE112AAA7%7D\r\n");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"An error occurred while scraping PDFs: {ex.Message}");
-    }
+    var scraper = scope.ServiceProvider.GetRequiredService<Scraper>();
+    await scraper.Scrape(startId, endId);
+}
+
+async Task RemoveDuplicatesAsync(IServiceProvider services)
+{
+    using var scope = services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<MrDb>();
+
+    var duplicates = await context.Forskolans
+        .GroupBy(f => new { f.Namn, f.Adress })
+        .Where(g => g.Count() > 1)
+        .SelectMany(g => g.OrderBy(x => x.Id).Skip(1))
+        .ToListAsync();
+
+    context.Forskolans.RemoveRange(duplicates);
+    await context.SaveChangesAsync();
 }
