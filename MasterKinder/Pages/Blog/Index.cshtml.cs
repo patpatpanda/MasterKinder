57f9833706a58ca1355a9fe2d24e946cb600e519
patpatpanda <nils-emil1337@hotmail.se>
2024-07-17 15:39:52 +0200

    a

--- MasterKinder/Pages/Blog/Index.cshtml.cs
+++ MasterKinder/Pages/Blog/Index.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using MasterKinder.Data;
+using MasterKinder.Models;
 
 namespace MasterKinder.Pages.Blog
 {
     public class IndexModel : PageModel
     {
+        private readonly MrDb _context;
+
+        public IndexModel(MrDb context)
+        {
+            _context = context;
+        }
+
+        public List<BlogPost> BlogPosts { get; set; }
+
         public void OnGet()
         {
+            BlogPosts = _context.BlogPosts.OrderByDescending(post => post.PublishedDate).ToList();
         }
     }
 }
