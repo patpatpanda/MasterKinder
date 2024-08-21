63f432f0be61a3dcbe97cdbb606a82bb034eaa38
patpatpanda <nils-emil1337@hotmail.se>
2024-08-21 10:53:20 +0200

    a

--- MasterKinder/Models/ISurveyResponse.cs
+++ MasterKinder/Models/ISurveyResponse.cs
@@ -3,12 +3,12 @@
     public interface ISurveyResponse
     {
         int Id { get; set; }
-        string Forskoleverksamhet { get; set; }
-        string Fragetext { get; set; }
-        string GraderingSvarsalternativ { get; set; }
-        string Utfall { get; set; }
-        string SvarsalternativText { get; set; }
-       string FrageNr { get; set; }
+        string? Forskoleverksamhet { get; set; }
+        string? Fragetext { get; set; }
+        string? GraderingSvarsalternativ { get; set; }
+        string? Utfall { get; set; }
+        string? SvarsalternativText { get; set; }
+       string? FrageNr { get; set; }
     }
    
 }
