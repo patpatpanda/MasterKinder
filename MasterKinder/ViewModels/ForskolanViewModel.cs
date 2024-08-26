7fc243257d5eab0c39cf1e48b5f08678a7ed5117
patpatpanda <nils-emil1337@hotmail.se>
2024-08-26 12:21:32 +0200

    aa

--- MasterKinder/ViewModels/ForskolanViewModel.cs
+++ MasterKinder/ViewModels/ForskolanViewModel.cs
@@ -1,26 +1,26 @@
-﻿//namespace MasterKinder.ViewModels
-//{
-//    public class ForskolanViewModel
-//    {
-//        public int Id { get; set; }
-//        public string? Namn { get; set; }
-//        public string? Adress { get; set; }
-//        public string? Beskrivning { get; set; }
-//        public string? TypAvService { get; set; }
-//        public string? VerksamI { get; set; }
-//        public string? Organisationsform { get; set; }
-//        public int AntalBarn { get; set; }
-//        public double? AntalBarnPerArsarbetare { get; set; }
-//        public double? AndelLegitimeradeForskollarare { get; set; }
-//        public string? Webbplats { get; set; }
-//        public string? InriktningOchProfil { get; set; }
-//        public string? InneOchUtemiljo { get; set; }
-//        public string? KostOchMaltider { get; set; }
-//        public string? MalOchVision { get; set; }
-//        public string? MerOmOss { get; set; }
-//        public ICollection<KontaktInfoViewModel>? Kontakter { get; set; }
-//        public double Latitude { get; set; }
-//        public double Longitude { get; set; }
-//    }
+﻿namespace MasterKinder.ViewModels
+{
+    public class ForskolanViewModel
+    {
+        public int Id { get; set; }
+        public string? Namn { get; set; }
+        public string? Adress { get; set; }
+        public string? Beskrivning { get; set; }
+        public string? TypAvService { get; set; }
+        public string? VerksamI { get; set; }
+        public string? Organisationsform { get; set; }
+        public int AntalBarn { get; set; }
+        public double? AntalBarnPerArsarbetare { get; set; }
+        public double? AndelLegitimeradeForskollarare { get; set; }
+        public string? Webbplats { get; set; }
+        public string? InriktningOchProfil { get; set; }
+        public string? InneOchUtemiljo { get; set; }
+        public string? KostOchMaltider { get; set; }
+        public string? MalOchVision { get; set; }
+        public string? MerOmOss { get; set; }
+        public ICollection<KontaktInfoViewModel>? Kontakter { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
 
-//}
+}
--- MasterKinder/ViewModels/KontaktInfoViewModel.cs
+++ MasterKinder/ViewModels/KontaktInfoViewModel.cs
@@ -1,11 +1,11 @@
-﻿//namespace MasterKinder.ViewModels
-//{
-//     public class KontaktInfoViewModel
-//    {
-//        public int Id { get; set; }
-//        public string? Namn { get; set; }
-//        public string? Epost { get; set; }
-//        public string? Telefon { get; set; }
-//        public string? Roll { get; set; }
-//    }
-//}
+﻿namespace MasterKinder.ViewModels
+{
+     public class KontaktInfoViewModel
+    {
+        public int Id { get; set; }
+        public string? Namn { get; set; }
+        public string? Epost { get; set; }
+        public string? Telefon { get; set; }
+        public string? Roll { get; set; }
+    }
+}
