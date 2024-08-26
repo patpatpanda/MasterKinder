b98168192484f5bd6dcc08c33a2cf05441646544
patpatpanda <nils-emil1337@hotmail.se>
2024-08-26 10:13:42 +0200

    a

--- MasterKinder/Models/Malibu.cs
+++ MasterKinder/Models/Malibu.cs
@@ -1,10 +1,10 @@
 ﻿public class Malibu
 {
     public int Id { get; set; }
-    public string Namn { get; set; }
-    public string Helhetsomdome { get; set; }
-    public int Svarsfrekvens { get; set; }
-    public int AntalSvar { get; set; }
+    public string? Namn { get; set; }
+    public string? Helhetsomdome { get; set; }
+    public int? Svarsfrekvens { get; set; }
+    public int? AntalSvar { get; set; }
     public string? NormalizedNamn { get; set; }
     public List<Question> Questions { get; set; }
 }
@@ -12,9 +12,9 @@
 public class Question
 {
     public int Id { get; set; }
-    public string FrageText { get; set; }
-    public int AndelInstammer { get; set; }
-    public int Year { get; set; }
+    public string? FrageText { get; set; }
+    public int? AndelInstammer { get; set; }
+    public int? Year { get; set; }
     public int MalibuId { get; set; }
     public Malibu Malibu { get; set; }
 }
--- MasterKinder/Models/PdfData.cs
+++ MasterKinder/Models/PdfData.cs
@@ -5,10 +5,10 @@ public class PdfData
     [Key]
     public int Id { get; set; }
 
-    public string Namn { get; set; }
-    public string Helhetsomdome { get; set; }
-    public int Svarsfrekvens { get; set; }
-    public int AntalSvar { get; set; }
+    public string? Namn { get; set; }
+    public string? Helhetsomdome { get; set; }
+    public int? Svarsfrekvens { get; set; }
+    public int? AntalSvar { get; set; }
     public string? NormalizedNamn { get; set; }
 
    
